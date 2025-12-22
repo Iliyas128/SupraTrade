8e291e460605f79d59445452dd97bf654577ac2f
Iliyas128 <weking128@gmail.com>
2025-12-22 19:48:35 +0500

    faviconAndImage

--- src/components/sections/HeroSlider.tsx
+++ src/components/sections/HeroSlider.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import heroMetal from "@/assets/goupgo_11zon.png";
+import heroMetal from "@/assets/goupgo.jpg";
 import { useNavigate } from "react-router-dom";
 import defenseMiniIcon from "@/assets/HeroIcons/defenseMiniIcon.png";
 import rostMiniIcon from "@/assets/HeroIcons/RostMiniIcon.png";
